b7503d83fe2b49e79395d3aaa366d9dee8651cee
Alvin Pratama <palveeen22@gmail.com>
2024-06-23 21:12:13 +0700

    git commit "perf: adjs performance column"

---
+++ WeatherApp/Components/WeatherColumn.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherColumn.swift
+//  WeatherApp
+//
+//  Created by Alvin Putra Pratama on 25/04/24.
+//
+
+import SwiftUI
+
+struct WeatherColumn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherColumn()
+}
